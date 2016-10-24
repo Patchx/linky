e50d2c7963c367875e56ca4d98908fb7d7ae18d4
Patchx <robert.anderson.fl@gmail.com>
2016-10-23 20:22:24 -0400

    work in progress

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,43 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@extends('layouts.default-layout')
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+@section('content')
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+<style>
+    html, body {
+        height: 100%;
+    }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+    body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        display: table;
+        font-weight: 100;
+        font-family: 'Lato';
+    }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+    .container {
+        text-align: center;
+        display: table-cell;
+        vertical-align: middle;
+    }
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
-    </body>
-</html>
+    .content {
+        text-align: center;
+        display: inline-block;
+    }
+
+    .title {
+        font-size: 96px;
+    }
+</style>
+
+
+<div class="container">
+    <div class="content">
+        <div class="title">Linky</div>
+    </div>
+</div>
+
+@endsection
