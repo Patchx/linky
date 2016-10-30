43e6e706debef520543e70b1dd8287bd821eddea
Patchx <robert.anderson.fl@gmail.com>
2016-10-30 17:06:08 -0400

    working on sizing tweaks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <h2 class="welcome-subtitle">Share better</h2>
         <div class="clearfix" style="padding-bottom:70px"></div>
 
-        <a href="#" class="btn btn-primary" style="color:white; font-size:30px">Get Started</a>
+        <a href="#" class="btn btn-primary" style="color:white; font-size:25px">Get Started</a>
         <div class="clearfix" style="padding-bottom:30px"></div>
 
         <a href="/demo">See the demo</a>
