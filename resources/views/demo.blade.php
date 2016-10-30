1890420b309faa13f7765037d879116b617612a7
Patchx <robert.anderson.fl@gmail.com>
2016-10-30 18:38:30 -0400

    fade out effect added for menu buttons

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -6,11 +6,11 @@
 <div>
 	<div class="top-banner">
 		<div class="banner-title">
-			<a href="#" class="pull-left banner-button">
+			<a href="#" class="pull-left banner-button fadeOut">
 				<span class="glyphicon glyphicon-list"></span>
 			</a>
 			<a href="/">Linky</a>
-			<a href="#" class="pull-right banner-button">
+			<a href="#" class="pull-right banner-button fadeOut">
 				<span class="glyphicon glyphicon-comment"></span>
 			</a>
 		</div>
@@ -23,5 +23,11 @@
 <div class="scroll-wrapper">
 	<iframe style="width:100%; height:90vh" src="https://www.hotels.com/hotel/details.html?pa=4&tab=description&hotel-id=346635&q-room-0-adults=2&ZSX=0&SYE=3&q-room-0-children=0"></iframe>
 </div>
+
+<script>
+	$('.banner-title .fadeOut').on('click',function(){
+		$('iframe').fadeTo("slow", 0.33);
+	});
+</script>
 
 @endsection
