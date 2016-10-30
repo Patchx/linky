3b0ea32d7e65faf808f7e2566f70879e6dfc2f80
Patchx <robert.anderson.fl@gmail.com>
2016-10-30 16:57:36 -0400

    working on the demo

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -9,6 +9,6 @@
 	</div>
 </div>
 
-<iframe style="width:100%; height:90vh" src="https://www.w3schools.com"></iframe> 
+<iframe style="width:100%; height:90vh" src="https://www.hotels.com/hotel/details.html?pa=4&tab=description&hotel-id=346635&q-room-0-adults=2&ZSX=0&SYE=3&q-room-0-children=0"></iframe> 
 
 @endsection
