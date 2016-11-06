6e66a1d848e21a165616d4c3d190c834048422b1
Patchx <robert.anderson.fl@gmail.com>
2016-11-06 11:56:04 -0500

    changing fonts and font style links

--- resources/views/layouts/welcome-layout.blade.php
+++ resources/views/layouts/welcome-layout.blade.php
@@ -30,11 +30,11 @@
   <!-- Custom styles for this template -->
   <link href="./css/custom.css" rel="stylesheet">
 
-  <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+  {{-- Fonts --}}
 
   <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
 
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet"> 
 
   </head>
 
