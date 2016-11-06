7b8245ea451ec123671c58d628a535062d52ae5a
Patchx <robert.anderson.fl@gmail.com>
2016-11-06 17:07:04 -0500

    slight wording tweak in the demo

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -37,9 +37,9 @@
 
 			<div class="modal-body">
 				<ol>
-					<li><a target="main-iframe" class="list-link" href="https://www.hotels.com/hotel/details.html?pa=4&tab=description&hotel-id=346635&q-room-0-adults=2&ZSX=0&SYE=3&q-room-0-children=0">Remm Hibiya</a></li>
+					<li><a target="main-iframe" class="list-link" href="https://www.hotels.com/hotel/details.html?pa=4&tab=description&hotel-id=346635&q-room-0-adults=2&ZSX=0&SYE=3&q-room-0-children=0">Hotel Remm Hibiya</a></li>
 
-					<li><a target="main-iframe" class="list-link" href="https://www.booking.com/hotel/jp/hoshinoya-tokyo.html">Hoshinoya Tokyo</a></li>
+					<li><a target="main-iframe" class="list-link" href="https://www.booking.com/hotel/jp/hoshinoya-tokyo.html">Hotel Hoshinoya Tokyo</a></li>
 
 					<li><a target="main-iframe" class="list-link" href="https://blog.hubspot.com/marketing/seo-friendly-blog-post-checklist-ht#sm.0001ly9bfim9gct4u9r12q9psmiau">SEO optimize your Blog</a></li>
 
